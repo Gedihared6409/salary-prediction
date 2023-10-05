a48afde2a89cc50975a18733ebec68ed428963fa
Gedihared6409 <gediali019@gmail.com>
2023-10-06 00:13:32 +0900

    initial commit

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from predict_page import show_predict_page
 from explore_page import show_explore_page
 
 
-page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+page = st.sidebar.selectbox("Explore Or Predict", ( "Explore", "Predict"))
 
-if page == "Predict":
-    show_predict_page()
-else:
+if page == "Explore":
     show_explore_page()
+else:
+    
+    show_predict_page()
